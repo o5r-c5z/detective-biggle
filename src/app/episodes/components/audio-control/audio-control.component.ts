e6e8536e9b18d5f79664509eee018df54f681ce2
gtolivier <olivier@charvoz.eu>
2025-03-02 12:35:20 +0100

    Simplify audio control component

--- src/app/episodes/components/audio-control/audio-control.component.ts
+++ src/app/episodes/components/audio-control/audio-control.component.ts
@@ -1,17 +1,11 @@
 import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
-import {
-  Component,
-  HostListener,
-  inject,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faVolumeHigh, faVolumeXmark } from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs';
 import { AnnouncementService } from '../../services/announcement.service';
 import { AudioService } from '../../services/audio.service';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faVolumeXmark, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-audio-control',
@@ -27,89 +21,33 @@ export class AudioControlComponent implements OnInit, OnDestroy {
 
   protected volume = 0.5;
   protected isPlaying$ = this.audioService.isPlaying$;
-  protected Math = Math; // Make Math available to the template
   protected showKeyboardHint = false;
   protected faVolumeXmark = faVolumeXmark;
   protected faVolumeHigh = faVolumeHigh;
 
   ngOnInit(): void {
-    // Set initial volume from service
     this.volume = this.audioService.getVolume();
 
-    // Show keyboard hint for a few seconds on init
     this.showKeyboardHint = true;
     setTimeout(() => {
       this.showKeyboardHint = false;
     }, 5000);
 
-    // Listen for audio state changes to announce to screen readers
     this.audioSubscription = this.isPlaying$.subscribe((isPlaying) => {
       if (isPlaying) {
-        this.announcementService.announce('Background music started playing');
+        this.announcementService.announce('Musique de fond en cours');
       } else {
-        this.announcementService.announce('Background music paused');
+        this.announcementService.announce('Musique de fond mise en pause');
       }
     });
   }
 
-  @HostListener('document:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent): void {
-    // Ignore key events when focus is on input or text area elements
-    if (
-      document.activeElement instanceof HTMLInputElement ||
-      document.activeElement instanceof HTMLTextAreaElement ||
-      document.activeElement instanceof HTMLSelectElement
-    ) {
-      return;
-    }
-
-    switch (event.key.toLowerCase()) {
-      case 'm': // 'M' key for mute/unmute
-        this.toggleAudio();
-        event.preventDefault();
-        break;
-      case 'arrowup': // Up arrow to increase volume
-        this.adjustVolume(0.1);
-        event.preventDefault();
-        break;
-      case 'arrowdown': // Down arrow to decrease volume
-        this.adjustVolume(-0.1);
-        event.preventDefault();
-        break;
-    }
-  }
-
   protected toggleAudio(): void {
     this.audioService.toggle();
-    // Show keyboard hint briefly when manually toggling
     this.showKeyboardHint = true;
     setTimeout(() => {
       this.showKeyboardHint = false;
     }, 3000);
-  }
-
-  protected setVolume(value: number): void {
-    this.volume = value;
-    this.audioService.setVolume(value);
-
-    // Announce volume change to screen readers
-    this.announcementService.announce(this.getVolumeText());
-  }
-
-  private adjustVolume(delta: number): void {
-    const newVolume = Math.min(Math.max(this.volume + delta, 0), 1);
-    this.setVolume(newVolume);
-
-    // Show keyboard hint briefly when changing volume
-    this.showKeyboardHint = true;
-    setTimeout(() => {
-      this.showKeyboardHint = false;
-    }, 3000);
-  }
-
-  protected getVolumeText(): string {
-    const percentage = Math.round(this.volume * 100);
-    return `Volume ${percentage}%`;
   }
 
   ngOnDestroy(): void {
